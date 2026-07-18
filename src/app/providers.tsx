9c322ce4aadef22f3b4623e6dc78e8c0f564d5e2
863683348 <863683348@qq.com>
2026-07-18 23:36:27 +0800

    Global theme: migrate to CSS variables + ThemeContext

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,12 +2,15 @@
 
 import { SessionProvider } from "next-auth/react";
 import { LangProvider } from "@/components/lang-context";
+import { ThemeProvider } from "@/components/theme-context";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <LangProvider>
-        {children}
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
       </LangProvider>
     </SessionProvider>
   );
